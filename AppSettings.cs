198aa8b32363b26b3cb78af4b5342f8f14104faf
a4shopno <a4shopno@gmail.com>
2024-07-13 17:04:33 +0600

    Create Microservices

---
+++ AppSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class AppSettings
+{
+    public JwtSettings JwtSettings { get; set; }
+    public RabbitMQSettings RabbitMQSettings { get; set; }
+    public RedisSettings RedisSettings { get; set; }
+    public string YodleeApiUrl { get; set; }
+    public string YodleeApiKey { get; set; }
+}
